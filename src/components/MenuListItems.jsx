d20d80f3e1fbfdc2b398bf9d57bb85f2470d27b0
nilpeep <niluferk038@gmail.com>
2024-07-21 22:53:40 +0300

    route

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,85 +1,75 @@
-import React, { useState } from 'react'
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard"
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
-import InventoryIcon from "@mui/icons-material/Inventory"
-import StoreIcon from "@mui/icons-material/Store"
-import StarsIcon from "@mui/icons-material/Stars"
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
-import {  useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
 
 const icons = [
-    {
-      icon: <SpaceDashboardIcon />,
-      title: "Dashboard",
-      url: "/stock/",
-    },
-    {
-      title: "Purchase",
-      icon: <ShoppingCartIcon />,
-      url: "/stock/purchases/",
-    },
-    {
-      title: "Sales",
-      icon: <AttachMoneyIcon />,
-      url: "/stock/sales/",
-    },
-    {
-      title: "Firms",
-      icon: <StoreIcon />,
-      url: "/stock/firms/",
-    },
-    {
-      title: "Brands",
-      icon: <StarsIcon />,
-      url: "/stock/brands/",
-    },
-    {
-      title: "Products",
-      icon: <InventoryIcon />,
-      url: "/stock/products/",
-    },
-  ]
+  {
+    title: "Purchase",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+];
 
 export const MenuListItems = () => {
-  const [hovered, setHovered] = useState(null)
-    const navigate = useNavigate()
+  const [hovered, setHovered] = useState(null);
+  const navigate = useNavigate();
 
-    const handleClick = (url,index) =>{
-      navigate(url)
-      setActive(index)
-    }
+  const handleClick = (url, index) => {
+    navigate(url);
+    setActive(index);
+  };
 
-    const [active, setActive] = useState(null)
+  const [active, setActive] = useState(null);
   return (
     <List>
-        {icons.map((item, index) => (
-   
-
-          <ListItem 
+      {icons.map((item, index) => (
+        <ListItem
           sx={{
-            color:active == index ? 'red' : 'white', 
-            "&:hover":{color:'red'},
-            "&:hover .MuiSvgIcon-root":{color:'red'},
-            "& .MuiSvgIcon-root":{color:active==index ? 'red' : 'white'},
-            
-            
-          }} 
-          key={index} disablePadding onClick={()=>handleClick(item.url,index)}>
-            <ListItemButton >
-              <ListItemIcon sx={{color:'white'}} >
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText primary={item.title} />
-            </ListItemButton>
-          </ListItem>
-          
-        ))}
-      </List>
-  )
-}
+            color: active == index ? "red" : "white",
+            "&:hover": { color: "red" },
+            "&:hover .MuiSvgIcon-root": { color: "red" },
+            "& .MuiSvgIcon-root": { color: active == index ? "red" : "white" },
+          }}
+          key={index}
+          disablePadding
+          onClick={() => handleClick(item.url, index)}
+        >
+          <ListItemButton>
+            <ListItemIcon sx={{ color: "white" }}>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.title} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
--- src/services/useLoginService.js
+++ src/services/useLoginService.js
@@ -1,73 +1,73 @@
 // import axios from "axios"
-import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
-import { useNavigate } from "react-router-dom"
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
+import { useNavigate } from "react-router-dom";
 import {
   fetchFail,
   fetchStart,
   loginSuccess,
   logoutSuccess,
   registerSuccess,
-} from "../features/authSlice"
-import { useDispatch } from "react-redux"
+} from "../features/authSlice";
+import { useDispatch } from "react-redux";
 // import {  useSelector } from "react-redux"
-import useAxios from "./useAxios"
+import useAxios from "./useAxios";
 
 const useLoginService = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   // const { token } = useSelector((state) => state.auth)
-  const { axiosWithToken, axiosPublic } = useAxios()
+  const { axiosWithToken, axiosPublic } = useAxios();
 
   const login = async (userInfo) => {
-    dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
       // const { data } = await axios.post(
       //   `${process.env.REACT_APP_BASE_URL}/auth/login/`,
       //   userInfo
       // )
-      const { data } = await axiosPublic.post("/auth/login/", userInfo)
-      dispatch(loginSuccess(data))
-      toastSuccessNotify("Login işlemi basarili.")
-      navigate("/stock")
+      const { data } = await axiosPublic.post("/auth/login/", userInfo);
+      dispatch(loginSuccess(data));
+      toastSuccessNotify("Login işlemi basarili.");
+      navigate("/stock/purchases");
     } catch (error) {
-      dispatch(fetchFail())
-      toastErrorNotify("Login işlemi başarisiz oldu.")
-      console.log(error)
+      dispatch(fetchFail());
+      toastErrorNotify("Login işlemi başarisiz oldu.");
+      console.log(error);
     }
-  }
+  };
 
   const register = async (userInfo) => {
-    dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
       // const { data } = await axios.post(
       //   `${process.env.REACT_APP_BASE_URL}/users/`,
       //   userInfo
       // )
-      const { data } = await axiosPublic.post("/users/", userInfo)
-      dispatch(registerSuccess(data))
-      navigate("/stock")
+      const { data } = await axiosPublic.post("/users/", userInfo);
+      dispatch(registerSuccess(data));
+      navigate("/stock");
     } catch (error) {
-      dispatch(fetchFail())
+      dispatch(fetchFail());
     }
-  }
+  };
 
   const logout = async () => {
-    dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
       // await axios.get(`${process.env.REACT_APP_BASE_URL}/auth/logout`, {
       //   headers: { Authorization: `Token ${token}` },
       // })
-      await axiosWithToken("/auth/logout/")
-      toastSuccessNotify("Çıkış işlemi başarili.")
-      dispatch(logoutSuccess())
+      await axiosWithToken("/auth/logout/");
+      toastSuccessNotify("Çıkış işlemi başarili.");
+      dispatch(logoutSuccess());
       // navigate("/")
     } catch (error) {
-      dispatch(fetchFail())
-      toastErrorNotify("Çıkış işlemi başarisiz oldu.")
+      dispatch(fetchFail());
+      toastErrorNotify("Çıkış işlemi başarisiz oldu.");
     }
-  }
+  };
 
-  return { login, register, logout }
-}
+  return { login, register, logout };
+};
 
-export {useLoginService}
+export { useLoginService };
